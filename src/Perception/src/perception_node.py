fab9684ad62c747d1e56692c07c5e1a69de060c6
omarmasoud <omark897@gmail.com>
2023-06-14 17:58:55 +0300

    refactored perception node to classes and object, axis consensus remaining yet

--- src/Perception/src/perception_node.py
+++ src/Perception/src/perception_node.py
@@ -26,112 +26,154 @@ import open3d as o3d
 import numpy as np
 import time
 
+# importing robot helper for coordinates view transformation 
 
-#load model 
+sys.path.append("../../control/src/")
+from robot_helper import RobotControl, frames_transformations
+import numpy as np
+import tf.transformations
 
-model = attempt_load("/home/omar/Desktop/GP/EwasteNonDestructiveDisassembly/src/Perception/src/epoch_480.pt","cpu")
-model = TracedModel(model,"cpu",640)
+class PerceptionNode():
+    def loadModel(location="/home/omar/Desktop/GP/EwasteNonDestructiveDisassembly/src/Perception/src/epoch_480.pt"): 
+        #load model 
 
-#print(model)
+        model = attempt_load(location,"cpu")
+        model = TracedModel(model,"cpu",640)
 
 
 
+    def __init__(self) -> None:
+        self.perceptionNode=rospy.init_node("Perception")
+        self.perceptionPublisher=rospy.Publisher('ListOfScrews' ,String,queue_size=50)
+        self.rate=rospy.Rate(200)
+        pass
 
-perceptionNode=rospy.init_node("Perception")
-perceptionPublisher=rospy.Publisher('ListOfScrews' ,String,queue_size=50)
-rate=rospy.Rate(50)
-# Configure camera streams
-pipeline = rs.pipeline()
-config = rs.config()
-config.enable_stream(rs.stream.color, 1280, 720, rs.format.bgr8, 30)
-config.enable_stream(rs.stream.depth, 848, 480, rs.format.z16, 30)
-pipeline.start(config)
-#camera viewing dimensions
-W=1280
-H=720
-colors_hash={}
-aligned_stream = rs.align(rs.stream.color) # alignment between color and depth
-point_cloud = rs.pointcloud()
-## Configure Camera Filters:
-temporalFilter=rs.temporal_filter()
-holeFillingFilter=rs.hole_filling_filter()
-while not rospy.is_shutdown():
-   
-    screwlist=[]
-    ## Camera Readings
-    frames = pipeline.wait_for_frames()
-    color_frame = frames.get_color_frame()
+    def InitializeCamera(self):
+
+        # Configure camera streams
+        self.pipeline = rs.pipeline()
+        self.config = rs.config()
+        self.config.enable_stream(rs.stream.color, 1280, 720, rs.format.bgr8, 30)
+        self.config.enable_stream(rs.stream.depth, 848, 480, rs.format.z16, 30)
+        self.pipeline.start(self.config)
+        #camera viewing dimensions
+        self.Width=1280
+        self.Height=720
+        #colors_hash={}
+        self.aligned_stream = rs.align(rs.stream.color) # alignment between color and depth
+        self.point_cloud = rs.pointcloud()
+        ## Configure Camera Filters:
+        self.temporalFilter=rs.temporal_filter()
+        self.holeFillingFilter=rs.hole_filling_filter()
+    def imagePreprocess(self):
+        frames = self.pipeline.wait_for_frames()
+        color_frame = frames.get_color_frame()
+        
+        self.color_image = np.asanyarray(color_frame.get_data())
+        
+        frames = self.aligned_stream.process(frames)
+        depth_frame = frames.get_depth_frame()
+        ## depth enhancement using temporal filtering and hole filling
+        depth_frame=self.temporalFilter.process(depth_frame)
+        depth_frame=self.holeFillingFilter.process(depth_frame)
+        ####
+        df = np.asarray(depth_frame.get_data())
+        points = self.point_cloud.calculate(depth_frame)
+        verts = np.asanyarray(points.get_vertices()).view(np.float32).reshape(-1, W, 3)  # xyz
+
+        
+        
+        image =self.color_image
+        #preprocesses the image and makes the image size a multiple of the stride size,resizes the image
+        image = letterbox(image)[0]
+
+
+        #transpose from cv2 bgr to rgb
+        image = image[:, :, ::-1].transpose(2, 0, 1)
+        image = np.ascontiguousarray(image)
+        image = torch.from_numpy(image).float()
+        image /= 255.0
+        image.unsqueeze(0)
+        image = image[None,:,:,:]
+        return image
+    def modelInference(self,image):
+        with torch.no_grad():   # Calculating gradients would cause a GPU memory leak
+            output = self.model(image,False)[0]
+
+        pred = non_max_suppression(output)
+       
+        predresults = pred[0]
+        #predresults format: (xtl,ytl,xbr,ybr,conf,class)
+        predresults[:, :4] = scale_coords(image.shape[2:], predresults[:, :4], image.shape).round()
+
+        return predresults
     
-    color_image = np.asanyarray(color_frame.get_data())
-    
-    frames = aligned_stream.process(frames)
-    depth_frame = frames.get_depth_frame()
-    ## depth enhancement
-    depth_frame=temporalFilter.process(depth_frame)
-    depth_frame=holeFillingFilter.process(depth_frame)
-    ####
-    df = np.asarray(depth_frame.get_data())
-    points = point_cloud.calculate(depth_frame)
-    verts = np.asanyarray(points.get_vertices()).view(np.float32).reshape(-1, W, 3)  # xyz
+    def pixelToSpace(self,predresults,debug=False):
+        screwlist=[]
+        for predresult in predresults:
+           
+            # Blue color in BGR
+            color = (255, 0, 0)
+            #print('predres value is {}'.format(predresult))
+            centerx = int((predresult[0]+predresult[2]))//2
+            centery = int((predresult[1]+predresult[3]))//2
+            print('center val is  {},{}',centerx,centery)
+            xyz_position = self.verts[centery][centerx]
+            xyz_position = [float(xyz_position[0]),float(xyz_position[1]),float(xyz_position[2])]
+            #print(xyz_position)
+            screwlist.append(xyz_position)
+            rc=cv2.rectangle(self.color_image,(int(predresult[0]),int(predresult[1])),(int(predresult[2]),int(predresult[3])),color,2)
+        if debug:
+            cv2.imshow('myimg',self.color_image)
+            cv2.waitKey(1)
 
-    ### Model inference on RGB
-    
-    img0 =color_image#cv2.imread("/content/gdrive/MyDrive/YOLO_V7/Test_images/test2.jpeg")
-    #preprocesses the image and makes the image size a multiple of the stride size,resizes the image
-    img = letterbox(img0)[0]
-  
-    #transpose from cv2 bgr to rgb
-    img = img[:, :, ::-1].transpose(2, 0, 1)
-    img = np.ascontiguousarray(img)
-    img = torch.from_numpy(img).float()
-    img /= 255.0
-    img.unsqueeze(0)
-    img = img[None,:,:,:]
-    
+        return screwlist
     
 
-    with torch.no_grad():   # Calculating gradients would cause a GPU memory leak
-        output = model(img,False)[0]
 
-    pred = non_max_suppression(output)
-    #print(pred[0].shape)
-    predresults = pred[0]
-    #predresults format: (xtl,ytl,xbr,ybr,conf,class)
-    predresults[:, :4] = scale_coords(img.shape[2:], predresults[:, :4], img0.shape).round()
+    def camToWorldPositions(self,x,y,z,verbose=False):
+        framesTransformer= frames_transformations()
+        static_frame=frames_transformations.put_frame_static_frame(parent_frame_name='camera_link',child_frame_name="static")
+        relative_transformations=framesTransformer.transform(parent_id="base_link",child_frame_id='static')#chid id might be wrong
+        # apply transformations and rotations wrt robot
+        translation=relative_transformations[:3]
+        
+        rotation=relative_transformations[3:]
+        cam_matrix=np.array([x,y,z,1])
+        
+        homogeneous_matrix=tf.transformations.compose_matrix(translate=translation,angles=rotation)
+       
+        worldpose=np.matmul(homogeneous_matrix,cam_matrix)
+        
+        return worldpose
     
-   
+    def publishReadings(self,Data):
 
-    ## pixel to world coordinates transformation
+        my_msg = String()
+        my_msg.data = json.dumps(Data)
+        self.perceptionPublisher.publish(my_msg)
 
-    #print('shape of verts is {}',verts.shape)
-    for predresult in predresults:
-        list()
-        # Blue color in BGR
-        color = (255, 0, 0)
-        #print('predres value is {}'.format(predresult))
-        centerx = int((predresult[0]+predresult[2]))//2
-        centery = int((predresult[1]+predresult[3]))//2
-        print('center val is  {},{}',centerx,centery)
-        xyz_position = verts[centery][centerx]
-        xyz_position = [float(xyz_position[0]),float(xyz_position[1]),float(xyz_position[2])]
-        #print(xyz_position)
-        screwlist.append(xyz_position)
-        rc=cv2.rectangle(color_image,(int(predresult[0]),int(predresult[1])),(int(predresult[2]),int(predresult[3])),color,2)
-    cv2.imshow('myimg',color_image)
-    cv2.waitKey(1)
-    
+    def operate(self):
+        self.__init__()
+        self.InitializeCamera()
 
-    ## converting detections to rosmsg
+        while not rospy.is_shutdown():
+        
+            
+            ## Camera Readings
+            ImageData = self.imagePreprocess()
+            ## Model inference on image data
+            predictions = self.modelInference(ImageData)
+            ## Get Positions in the space of camera
+            screwPositionsToCamera = self.pixelToSpace(predictions)
+            ## Get Positions relative to the base link
+            screwPositions=list()
 
-    ## Todo : Modify this line correctly to publish the list
-    ##screwlist=Float32MultiArray(screwlist)
-    
-    ##publishing the screwlist
-    my_msg = String()
-    my_msg.data = json.dumps(screwlist)
-    
-    
-    perceptionPublisher.publish(my_msg)
+            for screwPosition in screwPositionsToCamera:
+                screwPositions.append(self.camToWorldPositions(screwPosition[0],screwPosition[1],screwPosition[2])[:3])
 
+            ## Publish readings
+             
+            self.publishReadings(screwPositions)
 
-    rate.sleep()
+            self.rate.sleep()
