ca1bd3ea1d7df9eeb86b1acc873b86e2ca2295b5
ewaste <ebrahimabdelghfar550@gmail.com>
2023-06-19 13:11:20 +0300

    new file:   src/control/src/collisionSafety.py 	new file:   src/control/src/test_publisher_h_1.py 	modified:   src/control/src/unscrewing.py 	new file:   src/ft_sensor_driver/during_unscrewing.png 	new file:   src/ft_sensor_driver/epsilon value for unscrewing.png 	modified:   src/ft_sensor_driver/launch/force_filtered.launch 	new file:   src/ft_sensor_driver/scripts/filters_test_1.py 	modified:   src/resources/src/enums/topics.py

---
+++ src/ft_sensor_driver/scripts/weighted_avg_filter.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+import rospy
+import numpy as np
+from geometry_msgs.msg import WrenchStamped, Wrench
+from geometry_msgs.msg import PointStamped
+from std_msgs.msg import Float64
+from enums.topics import Topics
+
+import matplotlib.pyplot as plt
+import math
+
+
+weightMultplier = 0.01
+
+FxFiltered = 0
+FyFiltered = 0
+FzFiltered = 0
+TxFiltered = 0
+TyFiltered = 0
+TzFiltered = 0
+
+
+
+def raw_wrench_sub(wrench_msg):
+
+    global weightMultplier, FxFiltered, FyFiltered, FzFiltered, TxFiltered, TyFiltered, TzFiltered
+    # Update sensor readings
+    Fx = wrench_msg.wrench.force.x
+    Fy = wrench_msg.wrench.force.y
+    Fz = wrench_msg.wrench.force.z
+    Tx = wrench_msg.wrench.torque.x
+    Ty = wrench_msg.wrench.torque.y
+    Tz = wrench_msg.wrench.torque.z
+    FxFiltered = weightMultplier * Fx + (1-weightMultplier) * FxFiltered
+    FyFiltered = weightMultplier * Fy + (1-weightMultplier) * FyFiltered
+    FzFiltered = weightMultplier * Fz + (1-weightMultplier) * FzFiltered
+    TxFiltered = weightMultplier * Tx + (1-weightMultplier) * TxFiltered
+    TyFiltered = weightMultplier * Ty + (1-weightMultplier) * TyFiltered
+    TzFiltered = weightMultplier * Tz + (1-weightMultplier) * TzFiltered
+    
+    wrench_msg.wrench.force.x = FxFiltered
+    wrench_msg.wrench.force.y = FyFiltered
+    wrench_msg.wrench.force.z = FzFiltered
+    wrench_msg.wrench.torque.x = TxFiltered
+    wrench_msg.wrench.torque.y = TyFiltered
+    wrench_msg.wrench.torque.z = TzFiltered
+     
+    weighted_filtered_pub.publish(wrench_msg)
+
+
+rospy.init_node("ft_lpr")
+rospy.Subscriber("ft_sensor_wrench/wrench/raw", WrenchStamped, raw_wrench_sub)
+weighted_filtered_pub = rospy.Publisher(Topics.ForceSensorWrenchWeightedFilter.value, WrenchStamped, queue_size=1)
+rospy.spin()
