1e78a41501e06bac71d74789befc307a1ab6557f
ebrahimabdelghfar <ebrahimabdelghfar550@gmail.com>
2023-06-20 03:38:21 +0300

    new file:   src/CentralNode/src/starterSystem.py 	modified:   src/resources/src/enums/nodes.py

--- src/resources/src/enums/nodes.py
+++ src/resources/src/enums/nodes.py
@@ -14,10 +14,19 @@ class Nodes(Enum):
     MILLING = "MILLING"
     HeartBeat = "HeartBeat"
     #list that contains all the nodes that always should be running
-    # NodesToBeOperated=[FORCE,COLLISION_DETECTION,CENTRAL,VISION,CLAMPING,CHANGE_TOOL,CHECK_MOTOR,APPROACH_AND_ENGAGE,UNSCREW,MILLING]
-    NodesToBeOperated=[CHECK_MOTOR]
-    NodesToBeOperatedInfo = {CHECK_MOTOR:["checkMotors","checkMotorsNode.py",CHECK_MOTOR]}
-
-if __name__ == "__main__":
-    print("Hello, World!")
-    print(Nodes.NodesToBeOperated.value)
+    NodesToBeOperated=[FORCE,COLLISION_DETECTION,CENTRAL,VISION,CLAMPING,CHANGE_TOOL,CHECK_MOTOR,APPROACH_AND_ENGAGE,UNSCREW,MILLING]
+    #dictionary that contains the information about the nodes [pkgname , executable , node name]
+    NodesToBeOperatedInfo = {CHECK_MOTOR:[ "checkMotors" , "checkMotorsNode.py" ,CHECK_MOTOR ],
+                             HeartBeat: ["CentralNode" , "heartBeat.py" , HeartBeat ],
+                             CHANGE_TOOL:["control","ChangeToolNode.py",CHANGE_TOOL],
+                            }
+    StartSequance=[CENTRAL,
+                   CHECK_MOTOR,
+                   VISION,
+                   CLAMPING,
+                   CHANGE_TOOL,
+                   APPROACH_AND_ENGAGE,
+                   UNSCREW,
+                   MILLING,
+                   COLLISION_DETECTION,
+                   HeartBeat]
